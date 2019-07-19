9bc1bfc915397ef16751a68dce9dc8318bd60496
Alex Swedenburg <github@delorted.com>
2019-07-19 09:55:21 -0600

    Power not times, dummy

--- ConfigUtil/main.c
+++ ConfigUtil/main.c
@@ -145,7 +145,7 @@ void get_ptt_status(unsigned char p)
 float fround(float f, uint digits)
 {
     if (digits == 0) return round(f);
-    return float(int(f * (10 * digits) + 0.5)) / (digits * 10);
+    return float(int(f * pow(10, digits) + 0.5)) / pow(10, digits);
 }
 
 void doBeacon()
