8b276c20f4e3dffaf37a4f650035c2ee35929846
Alex Swedenburg <github@delorted.com>
2020-05-06 19:25:09 -0600

    The config utility has grown into something larger. Moving it to its own repo.

--- ConfigUtil/beacon.h
+++
@@ -1,6 +0,0 @@
-#ifndef INC_BEACON_H
-#define INC_BEACON_H
-
-void doBeacon(unsigned int num);
-
-#endif
--- ConfigUtil/util.h
+++
@@ -1,8 +0,0 @@
-#ifndef INC_UTIL_H
-#define INC_UTIL_H
-
-float fround(float f, uint digits);
-float read_temp();
-float read_adc(int num, int scale);
-
-#endif
